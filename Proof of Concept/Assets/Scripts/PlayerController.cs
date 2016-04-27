7432220d05691afb0a486e8dc7b45652fe147f7f
teganv <teganv@uoregon.edu>
2016-04-26 21:44:48 -0700

    Player can move three 1 unit increments per turn. Enter ends the player's turn, backspace returns player to their starting position from the beginning of the turn and resets the spaces moved count. A turn manager script was added with a skeleton for an enemy turn function to be filled out

--- Proof of Concept/Assets/Scripts/PlayerController.cs
+++ Proof of Concept/Assets/Scripts/PlayerController.cs
@@ -6,30 +6,81 @@ public class PlayerController : MonoBehaviour
 
     public float playerSpeed = 3.0f;
 
+	public int maxMoveDistance = 3; //the most spaces the player can move in a turn
+
+	private bool turn = true; //the player's turn (as opposed to the enemy's)
+
+	private Vector3 movePosition;
+	private Vector3 startPosition;
+	private int spacesMoved = 0;
+
 	void Start()
     {
-	    
+		StartCoroutine (PlayerTurn ());
 	}
 	
 	
 	void Update()
     {
-        // FIXME: There's probably a more elegant way to move the player.
-        if(Input.GetKey(KeyCode.A))
-        {
-            transform.position = new Vector3(transform.position.x - playerSpeed * Time.deltaTime, transform.position.y, 0);
-        }
-        else if(Input.GetKey(KeyCode.D))
-        {
-            transform.position = new Vector3(transform.position.x + playerSpeed * Time.deltaTime, transform.position.y, 0);
-        }
-        else if(Input.GetKey(KeyCode.W))
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y + playerSpeed * Time.deltaTime);
-        }
-        else if(Input.GetKey(KeyCode.S))
-        {
-            transform.position =  new Vector3(transform.position.x, transform.position.y - playerSpeed * Time.deltaTime);
-        }
+
     }
+
+	IEnumerator PlayerTurn() {
+		movePosition = transform.position;
+		startPosition = transform.position;
+
+		//while it's the player's turn
+		while (TurnManager.playerTurn) {
+
+			//horizontal movement
+			if (Input.GetButtonDown ("Horizontal")) {
+				if (Input.GetAxisRaw ("Horizontal") > 0) {
+					AttemptMove (Vector2.right);
+				} else {
+					AttemptMove (Vector2.left);
+				}
+				yield return null;
+			//vertical movement
+			} else if (Input.GetButtonDown ("Vertical")) {
+				if (Input.GetAxisRaw ("Vertical") > 0) {
+					AttemptMove (Vector2.up);
+				} else {
+					AttemptMove (Vector2.down);
+				}
+				yield return null;
+
+			//end turn
+			} else if (Input.GetKeyDown (KeyCode.Return)) {
+				TurnManager.playerTurn = false;
+				spacesMoved = 0;
+				transform.position = movePosition;
+				yield return null;
+			} else if (Input.GetKeyDown (KeyCode.Backspace)) {
+				spacesMoved = 0;
+				movePosition = startPosition;
+				transform.position = movePosition;
+				yield return null;
+			}
+			yield return null;
+		}
+
+		yield break;
+	}
+
+
+	public void AttemptMove(Vector2 moveDirection) {
+		int unwalkable = 1 << 9;
+		RaycastHit2D hit = Physics2D.Raycast (new Vector2 (transform.position.x, transform.position.y), moveDirection, 1f, unwalkable);
+		//there's a wall in the way
+		if (hit.rigidbody != null) {
+			return;
+		}
+		//have we already moved the max number of spaces?
+		if (spacesMoved + 1 <= maxMoveDistance) {
+			spacesMoved++;
+			movePosition += new Vector3(moveDirection.x, moveDirection.y, 0);
+			transform.position = movePosition;
+		}
+		return;
+	}
 }
---
+++ Proof of Concept/Assets/Scripts/TurnManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//Handles the enemy's turn, and switching between player/enemy turns
+public class TurnManager : MonoBehaviour {
+
+	public static bool playerTurn = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!playerTurn) {
+			enemyTurn ();
+		}
+	}
+
+	private void enemyTurn() {
+		print ("Watch out folks, we're enemies and we're gonna getcha");
+		playerTurn = true;
+		//fill out with actual enemy movement stuff
+		return;
+	}
+}

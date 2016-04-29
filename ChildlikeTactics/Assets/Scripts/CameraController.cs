5fbea2d690e178d2f8e046e4eb0f21d66c4a5145
jalan <jember@uoregon.edu>
2016-04-29 12:47:02 -0700

    player now has their model attached
    I attached the player model to the player. I implemented this as a
    separate object that is a child of Player called PlayerRenderer, which
    has mesh and meshRenderer components. This method allows us to make any
    kind of visual adjustments without worrying about altering behavior
    (e.g. the player's model can be scaled and rotated without affecting the
    player object's collider). I also tweaked the camera and lighting a bit
    so things look nice and 3d

--- ChildlikeTactics/Assets/Scripts/CameraController.cs
+++ ChildlikeTactics/Assets/Scripts/CameraController.cs
@@ -8,12 +8,13 @@ public class CameraController : MonoBehaviour
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");        
     }
 
 	void Update()
     {
         // FIXME: There's probably a more elegant way to lock the camera to the player. 
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
-	}
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y - 12, transform.position.z);
+        transform.LookAt(player.transform);
+    }
 }

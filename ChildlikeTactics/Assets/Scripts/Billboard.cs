3f2241b62a610a823a3bc881bd045015347d68b9
jalan <jember@uoregon.edu>
2016-05-09 21:25:50 -0700

    healthbar billboard
    the health bar object has billboard behavior now

---
+++ ChildlikeTactics/Assets/Scripts/Billboard.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour {
+
+	Transform cameraTransform;
+
+	void Start(){
+		cameraTransform = Camera.main.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = cameraTransform.rotation;
+	}
+}

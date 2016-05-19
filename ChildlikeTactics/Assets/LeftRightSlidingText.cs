35fea0b53b2d3c5470274d88cf1f62bc238ce10c
Tegan <teganv@uoregon.edu>
2016-05-18 18:35:25 -0700

    Reverted the TurnManager script to how it was before, deleting the scrolling text stuff from it. Made a prefab for Scrolling text that can be instantiated to display any sort of message across the screen once some stuff gets fixed

---
+++ ChildlikeTactics/Assets/LeftRightSlidingText.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LeftRightSlidingText : MonoBehaviour {
+	
+	private RectTransform textTransform;
+	public Text text;
+
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+		textTransform = GetComponent<RectTransform> ();
+		StartCoroutine (scroll ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator scroll() {
+		while (textTransform.position.x < 900) {
+			print (textTransform.position.x.ToString ());
+			textTransform.position += new Vector3(400, 0, 0) * Time.deltaTime;
+			yield break;
+		}
+		Destroy (gameObject);
+		yield return null;
+	}
+}
--- ChildlikeTactics/Assets/Scripts/TurnManager.cs
+++ ChildlikeTactics/Assets/Scripts/TurnManager.cs
@@ -5,48 +5,27 @@ using UnityEngine.UI;
 
 //Handles the enemy's turn, and switching between player/enemy turns
 public class TurnManager : MonoBehaviour {
-	public GameObject enemyTurnText;
 
 
 	public static bool playerTurn = true;
-	//public bool playerTurnTest = true;
-
-	float startXPos;
-	RectTransform textTransform;
-
-
 
 	// Use this for initialization
 	void Start () {
-		textTransform = enemyTurnText.GetComponent<RectTransform> ();
-		startXPos = textTransform.position.x;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (!playerTurn) {
-			StartCoroutine (enemyTurnSwitch ());
+			enemyTurn();
 		}
 
     }
 
-	/*private void enemyTurn() {
+	private void enemyTurn() {
         this.GetComponent<EnemyManager>().EnemyTurn();
 		playerTurn = true;
 		return;
-	}*/
-
-
-	//Scrolls "Enemy Phase" Text across the screen and then starts the enemy turn
-	IEnumerator enemyTurnSwitch() {
-		while (textTransform.position.x < 900) {
-			print (textTransform.position.x.ToString ());
-			textTransform.position += new Vector3(400, 0, 0) * Time.deltaTime;
-			yield break;
-		}
-		textTransform.position = new Vector3 (startXPos, textTransform.position.y, textTransform.position.z);
-		this.GetComponent<EnemyManager>().EnemyTurn();
-		playerTurn = true;
-		yield return null;
 	}
+		
 }

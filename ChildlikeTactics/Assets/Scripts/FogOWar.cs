703e5c821e0712d2b219a46f4fb17d713bc55a4c
Kyle Sammons <ksammon2@uoregon.edu>
2016-05-20 15:45:46 -0700

    Readded the fuggin' FogOWarManager and made all 'fog' a child to clean up the hierarchy.

--- ChildlikeTactics/Assets/Scripts/FogOWar.cs
+++ ChildlikeTactics/Assets/Scripts/FogOWar.cs
@@ -5,18 +5,18 @@ public class FogOWar : MonoBehaviour
 {
     public GameObject fogOWar;
     public List<int> drawnFog;
-    public bool drawFogOfWar;
-    private GameObject[] fog;
 
-	// Use this for initialization
+    private GameObject[] fog;
+    private Transform fogParent;
+
 	void Start ()
     {
+        fogParent = new GameObject("Fog").transform;
         fog = new GameObject[Generate.rooms.Count];
         drawnFog = new List<int>();
         drawnFog.Add(0);
 	}
 	
-	// Update is called once per frame
 	void Update ()
     {
             updateFogOWar();
@@ -35,6 +35,7 @@ public class FogOWar : MonoBehaviour
             {
                 GameObject instance = (GameObject)Instantiate(fogOWar, new Vector3(room.x + room.width / 2, room.y + room.height / 2, -1.0f), Quaternion.Euler(-90, 0, 0));
                 instance.transform.localScale = new Vector3(0.1f * (room.width + 2), 1, 0.1f * (room.height + 2));
+                instance.transform.SetParent(fogParent);
                 fog[index] = instance;
                 drawnFog.Add(index);
             }

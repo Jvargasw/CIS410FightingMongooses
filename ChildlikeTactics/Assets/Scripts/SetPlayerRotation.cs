d2f3e60bcf3b7aadd32a371cd53331d2d19f3c90
jalan <jember@uoregon.edu>
2016-05-20 09:44:19 -0700

    them trees rotate

--- ChildlikeTactics/Assets/Scripts/SetPlayerRotation.cs
+++ ChildlikeTactics/Assets/Scripts/SetPlayerRotation.cs
@@ -3,8 +3,7 @@ using System.Collections;
 
 public class SetPlayerRotation : MonoBehaviour {
 
-	//this script sets PlayerRenderer's rotation to 270 around x
 	void Start () {
-		transform.rotation = Quaternion.AngleAxis (270f, new Vector3 (1, 0, 0));
+		transform.rotation *= Quaternion.AngleAxis (270f, new Vector3 (1, 0, 0));
 	}
 }
---
+++ ChildlikeTactics/Assets/Scripts/setRandomRotZ.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setRandomRotZ : MonoBehaviour {
+
+	void Start () {
+		transform.rotation = Quaternion.AngleAxis(Random.Range(0, 360), new Vector3(0, 0, 1));
+		transform.rotation *= Quaternion.AngleAxis(-90f, new Vector3(1, 0, 0));
+	}
+}

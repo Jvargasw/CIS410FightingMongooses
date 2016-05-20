0d6b98e08a3019a55a035ba231ba14cbbe28d061
Tegan <teganv@uoregon.edu>
2016-05-20 09:29:47 -0700

    lerpin that camera like it's nobody's business

--- ChildlikeTactics/Assets/Scripts/CameraController.cs
+++ ChildlikeTactics/Assets/Scripts/CameraController.cs
@@ -8,14 +8,20 @@ public class CameraController : MonoBehaviour
 
     void Start()
     {
-        player = GameObject.Find("ActiveUnitIndicator");        
+        player = GameObject.Find("ActiveUnitIndicator");    
+		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y - 12, transform.position.z);
+		transform.LookAt (player.transform);
     }
 
 	void Update()
     {
         // FIXME: There's probably a more elegant way to lock the camera to the player. 
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y - 12, transform.position.z);
-        transform.LookAt(player.transform);
+		Vector3 targetPos = new Vector3(player.transform.position.x, player.transform.position.y - 12, transform.position.z);
+		transform.position = Vector3.Lerp(transform.position, targetPos, .16f);
+		if (Vector3.Distance(transform.position, targetPos) < .04f) {
+			transform.position = targetPos;
+		}
+        //transform.LookAt(player.transform);
 
 
     }

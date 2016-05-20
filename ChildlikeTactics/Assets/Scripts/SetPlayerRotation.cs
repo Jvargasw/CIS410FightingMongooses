206d45327a8c8190e88d590163f8ec91eceb9335
jalan <jember@uoregon.edu>
2016-05-20 09:26:41 -0700

    player set rotation in script
    hopefully this will help avoid gimble lock

---
+++ ChildlikeTactics/Assets/Scripts/SetPlayerRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetPlayerRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.rotation = Quaternion.AngleAxis (270f, new Vector3 (1, 0, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

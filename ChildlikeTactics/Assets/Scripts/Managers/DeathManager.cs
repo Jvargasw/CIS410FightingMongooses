7b7db595fc2d6ebdbdf2c366ebef34adc134cb6a
Jvargasw <jvargasw@uoregon.edu>
2016-05-20 19:55:35 -0700

    Got the witty messages to show up on death
    yay

--- ChildlikeTactics/Assets/Scripts/Managers/DeathManager.cs
+++ ChildlikeTactics/Assets/Scripts/Managers/DeathManager.cs
@@ -7,11 +7,14 @@ using System.Collections;
 public class DeathManager : MonoBehaviour
 {
     /// TBA, commented out for warnings reasons
-    //string[] messageArray = new string[] { "Good work there kid, you're dead", "Nice going dingus", "A mind is a terrible thing to waste...", "What an immature way to die...", "Insert death message here" };
-    public GameObject DiedText;
+    string[] messageArray = new string[] { "Good work there kid, you're dead", "Nice going dingus", "A mind is a terrible thing to waste...", "What an immature way to die...", "Insert death message here" };
+    
     void OnEnable()
     {
         //change the text to somthing witty
+        Transform DiedText = transform.FindChild("DiedText");
+        Text t = DiedText.GetComponent<Text>();
+        t.text = messageArray[Random.Range(0, messageArray.Length)];
     }
     public void ExitGame()
     {

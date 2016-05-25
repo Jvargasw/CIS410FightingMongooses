30ea91c3d5282008166c830fe3ea5c27ad86dbef
jalan <jember@uoregon.edu>
2016-05-24 22:40:05 -0700

    fog o war is a cube now. Covers enemy health bars
    but it's too big imo :/

--- ChildlikeTactics/Assets/Scripts/FogOWar.cs
+++ ChildlikeTactics/Assets/Scripts/FogOWar.cs
@@ -26,8 +26,8 @@ public class FogOWar : MonoBehaviour
             int index = Generate.rooms.IndexOf(room);
             if (!drawnFog.Contains(index) && !room.containsPlayer)
             {
-                GameObject instance = (GameObject)Instantiate(fogOWar, new Vector3(room.x + room.width / 2, room.y + room.height / 2, -0.55f), Quaternion.Euler(-90, 0, 0));
-                instance.transform.localScale = new Vector3(0.1f * (room.width + 2), 1, 0.1f * (room.height + 2));
+                GameObject instance = (GameObject)Instantiate(fogOWar, new Vector3(room.x + room.width / 2, room.y + room.height / 2, 0f), Quaternion.Euler(-90, 0, 0));
+                instance.transform.localScale = new Vector3((room.width + 2), 3.5f, (room.height + 2));
                 instance.transform.SetParent(fogParent);
                 fog[index] = instance;
                 drawnFog.Add(index);

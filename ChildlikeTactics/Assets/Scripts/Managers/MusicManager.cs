834b2416eae53657b26e2621a67cf52497361c67
Jvargasw <jvargasw@uoregon.edu>
2016-05-27 02:19:20 -0700

    Music! With Transitions!
    Hopefully this carries over in version control as well, time to see.
    Sorry in advance for all the new files

---
+++ ChildlikeTactics/Assets/Scripts/Managers/MusicManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+	
+	FMODUnity.StudioEventEmitter emitter;
+	// Use this for initialization
+	GameObject manager;
+	void OnEnable () 
+	{
+		emitter = GetComponent<FMODUnity.StudioEventEmitter> ();
+		manager = GameObject.Find ("GameManager");
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		TurnManager managerScript = manager.GetComponent<TurnManager> ();
+
+		if( managerScript.inCombat == true)
+		{ 
+			emitter.SetParameter("Battle", 1);
+		
+		}
+
+		else
+		{
+			emitter.SetParameter("Battle", 0);
+		}
+	}
+}

9cd0bb87360cd30e0e2e371b29110fa2c2bf0f41
Tegan <teganv@uoregon.edu>
2016-06-06 14:23:00 -0700

    controls screen image should scale properly now

--- ChildlikeTactics/Assets/Scripts/FogOWar.cs
+++ ChildlikeTactics/Assets/Scripts/FogOWar.cs
@@ -28,14 +28,6 @@ public class FogOWar : MonoBehaviour
             int index = Generate.rooms.IndexOf(room);
             if (!drawnFog.Contains(index) && !room.containsPlayer)
             {
-				/*for (int i = 0; i < room.width; i++) {
-					for (int j = 0; j < room.height; j++) {
-						GameObject instance = (GameObject)Instantiate(fogOWar, new Vector3(room.x + i, room.y - j, 0f), Quaternion.Euler(-90, 0, 0));
-						instance.transform.SetParent(fogParent);
-						fog[index] = instance;
-						drawnFog.Add(index);
-					}
-				}*/
 
 				GameObject fadedFog = (GameObject)Instantiate(fadedFogOWar, new Vector3(room.x + room.width / 2, room.y + room.height / 2, 0f), Quaternion.Euler(-90, 0, 0));
 				GameObject solidFog = (GameObject)Instantiate(fogOWar, new Vector3(room.x + room.width / 2, room.y + room.height / 2, 0f), Quaternion.Euler(-90, 0, 0));

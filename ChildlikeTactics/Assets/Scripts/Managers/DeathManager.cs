86d41b5b663ed4275398bd3d167f3be68c999025
Jvargasw <jvargasw@uoregon.edu>
2016-06-06 12:04:27 -0700

    Fixed a broke
    such broke. now fixed. wow

--- ChildlikeTactics/Assets/Scripts/Managers/DeathManager.cs
+++ ChildlikeTactics/Assets/Scripts/Managers/DeathManager.cs
@@ -8,9 +8,10 @@ public class DeathManager : MonoBehaviour
 {
     /// TBA, commented out for warnings reasons
     string[] messageArray = new string[] { "Good work there kid, you're dead", "Nice going dingus", "A mind is a terrible thing to waste...", "What an immature way to die...", "Insert coin to continue" };
-    
+	GameObject cameraRef;
     void OnEnable()
     {
+		cameraRef = GameObject.Find ("Main Camera");
 		MusicManager musicScript = cameraRef.GetComponent<MusicManager> ();
         //change the text to somthing witty
 		musicScript.EndMusic();

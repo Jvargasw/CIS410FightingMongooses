59a112d83dc9e35e120ef2aefaa1d8ec77cd809e
Tegan <teganv@uoregon.edu>
2016-06-06 01:53:22 -0700

    fog of war now has two parts, a solid core and a transparent outer rim so room entrances aren't blocked from view

--- ChildlikeTactics/Assets/Scripts/FogOWar.cs
+++ ChildlikeTactics/Assets/Scripts/FogOWar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 public class FogOWar : MonoBehaviour
 {
-    public GameObject fogOWar;
+	public GameObject fogOWar, fadedFogOWar;
     public List<int> drawnFog;
 
     private GameObject[] fog;
@@ -12,25 +12,41 @@ public class FogOWar : MonoBehaviour
 	void Start ()
     {
         fogParent = new GameObject("Fog").transform;
-        fog = new GameObject[Generate.rooms.Count];
+		fog = new GameObject[Generate.rooms.Count * 2];
         drawnFog = new List<int>();
         drawnFog.Add(0);
         OnPlayerChangeRooms(0);
 	}
 	
-    void OnPlayerChangeRooms(int roomIndex)
+
+	void OnPlayerChangeRooms(int roomIndex)
     {
         Destroy(fog[roomIndex]);
+		Destroy(fog[Generate.rooms.Count + roomIndex]);
         foreach (Room room in Generate.rooms)
         {
             int index = Generate.rooms.IndexOf(room);
             if (!drawnFog.Contains(index) && !room.containsPlayer)
             {
-                GameObject instance = (GameObject)Instantiate(fogOWar, new Vector3(room.x + room.width / 2, room.y + room.height / 2, 0f), Quaternion.Euler(-90, 0, 0));
-                instance.transform.localScale = new Vector3(.9f * (room.width + 2), 3.5f, .9f * (room.height + 2));
-                instance.transform.SetParent(fogParent);
-                fog[index] = instance;
+				/*for (int i = 0; i < room.width; i++) {
+					for (int j = 0; j < room.height; j++) {
+						GameObject instance = (GameObject)Instantiate(fogOWar, new Vector3(room.x + i, room.y - j, 0f), Quaternion.Euler(-90, 0, 0));
+						instance.transform.SetParent(fogParent);
+						fog[index] = instance;
+						drawnFog.Add(index);
+					}
+				}*/
+
+				GameObject fadedFog = (GameObject)Instantiate(fadedFogOWar, new Vector3(room.x + room.width / 2, room.y + room.height / 2, 0f), Quaternion.Euler(-90, 0, 0));
+				GameObject solidFog = (GameObject)Instantiate(fogOWar, new Vector3(room.x + room.width / 2, room.y + room.height / 2, 0f), Quaternion.Euler(-90, 0, 0));
+				fadedFog.transform.localScale = new Vector3(.9f * (room.width + 2), 2.8f, .9f * (room.height + 2));
+				fadedFog.transform.SetParent(fogParent);
+				solidFog.transform.localScale = new Vector3(.9f * (room.width - 1.5f), 3.3f, .9f * (room.height - 2.5f));
+				solidFog.transform.SetParent(fogParent);
+				fog[index] = fadedFog;
+				fog [Generate.rooms.Count + index] = solidFog;
                 drawnFog.Add(index);
+                
             }
         }
     }
